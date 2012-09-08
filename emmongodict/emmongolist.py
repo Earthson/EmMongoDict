fb05dcdac7b735e5582c47052d334007628471db
Earthson Lu <Earthson.Lu@gmail.com>
2012-09-08 23:05:55 +0800

    add emmongolist

---
+++ emmongodict/emmongolist.py
@@ -0,0 +1,105 @@
+from collectionbase import *
+
+class EmMongoList(object):
+    '''
+    '''
+    db_info = {
+        'db':'EmMongoDict',
+        'collection':'EmMongoDict',
+    }
+
+    def __init__(self, spec, path, db=None, collection=None):
+        '''path and spec should not be None'''
+        self.db_info = dict(EmMongoList.db_info)
+        if collection is not None:
+            self.db_info['collection'] = collection
+        if db is not None:
+            self.db_info['db'] = db
+        self.spec = spec
+        self.path = path
+
+    def is_exist(self):
+        '''test whether spec is in db'''
+        ret = collection_find_one(spec_or_id=self.spec, fields={'_id':1}, 
+                                                **self.db_info)
+        return ret is not None
+
+    def ensure_exist(self):
+        '''return: True, if exist before
+                   False, if not exist before
+        '''
+        if self.is_exist() is False:
+            collection_insert(doc_or_docs=self.spec, **self.db_info)
+            return False
+        return True
+
+    def __getitem__(self, key):
+        '''slice is not supported yet'''
+        ret = collection_find_one(spec_or_id=self.spec, 
+                    fields={self.path:{'$slice':[key, 1]}}, **self.db_info)
+        return get_dict_property(ret, self.path)[0]
+
+    def __setitem__(self, key, value):
+        '''slice is not supported yet'''
+        pos = self.path+'.'+str(key)
+        return collection_update(spec=self.spec, 
+                    document={'$set':{pos:value}}, **self.db_info)
+
+    def __delitem__(self, key):
+        pos = self.path+'.'+str(key)
+        return collection_update(spec=self.spec,
+                    document={'$unset':{pos:1}}, **self.db_info)
+
+    def get_slice(self, start, lim=None):
+        '''mongo style slice
+        if lim is None:
+            start is positive:
+                first start object will be return
+            start is negtive:
+                last start object will be return
+        else:
+            from start lim object will be return
+
+            (see doc of mongodb for more help...)
+        '''
+        if lim is None:
+            ret = collection_find_one(spec_or_id=self.spec,
+                    fields={self.path:{'$slice':start}}, **self.db_info)
+            return get_dict_property(ret, self.path)
+        ret = collection_find_one(spec_or_id=self.spec,
+                    fields={self.path:{'$slice':[start, lim]}}, **self.db_info)
+        return get_dict_property(ret, self.path)
+
+    def add_to_set(self, *objs):
+        return collection_update(spec=self.spec,
+                        document={'$addToSet':{self.path:{'$each':objs}}},
+                        **self.db_info)
+
+    def push(self, *objs):
+        return collection_update(spec=self.spec,
+                        document={'$pushAll':{self.path:objs}}, **self.db_info)
+
+    def pop(self):
+        return collection_update(spec=self.spec,
+                        document={'$pop':{self.path:1}}, **self.db_info)
+
+    def pop_head(self):
+        return collection_update(spec=self.spec,
+                        document={'$pop':{self.path:-1}}, **self.db_info)
+
+    def pull(self, *objs):
+        return collection_update(spec=self.spec,
+                        document={'pullAll':{self.path:objs}}, **self.db_info)
+
+    def __len__(self):
+        return len(self.load_list())
+
+    def load_list(self):
+        '''load list as an instance of list'''
+        ret = collection_find_one(spec_or_id=self.spec,
+                        fields={self.path:1}, **self.db_info)
+        return get_dict_property(ret, self.path)
+
+    def remove(self):
+        '''remove list from db'''
+        return collection_remove(spec_or_id=self.spec)
