6cd25bf7c21f3c7a5a2afc5446d8f88cb539d270
Earthson Lu <Earthson.Lu@gmail.com>
2012-10-28 23:04:45 +0800

    fix len of emmongidict

--- emmongodict/emmongodict.py
+++ emmongodict/emmongodict.py
@@ -211,7 +211,9 @@ class EmMongoDict(object):
 
     def __len__(self):
         doc = self.load_doc()
-        return len(doc)
+        if doc is not None:
+            return len(doc)
+        return 0
 
     def __contains__(self, item):
         return self[item] is not None
